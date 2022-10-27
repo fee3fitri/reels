9ab72991fd80248f8c53d237063856da93ee04c6
fee3fitri <safitri.shelton@gmail.com>
2022-10-27 14:42:05 -0700

    Finished front end auth setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-import configureStore from "./store"
+import configureStore from "./store";
+import csrfFetch, {restoreCSRF} from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 function Root() {
@@ -22,9 +24,17 @@ function Root() {
   );
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (sessionStorage.getItem('X-CSRF-Token') === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+}
