218f2a8bb9b3c60893e3152690dd809802a40b7e
fee3fitri <safitri.shelton@gmail.com>
2022-11-06 18:46:03 -0800

    show product color. Have styling bugs when selected

--- frontend/src/components/ProductItem/ProductColor.js
+++ frontend/src/components/ProductItem/ProductColor.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams,Link } from "react-router-dom";
-import { fetchProduct, loadProduct, toggleSize } from "../../store/products";
+import { fetchProduct, loadProduct, loadProducts, toggleSize } from "../../store/products";
 import './ProductItem.css'
 
 
@@ -9,12 +9,11 @@ const ProductColor = () => {
   const {productId} = useParams();
   const dispatch = useDispatch();
   const product = useSelector(loadProduct(productId));
-  const colors = product.color.split(' ');
+  const colors = product.color.toLowerCase().split(' ');
   const [color, setColor] = useState();
   const productName = product.name.toLowerCase().split(' ');
-  const productFile = productName.join('_');
   const name = productName[0];
-  const colorUrl = `https://reels-dev.s3.us-west-1.amazonaws.com/${name}/${productFile}_color.webp`;
+
 
   useEffect(() => {
     dispatch(fetchProduct(productId));
@@ -30,23 +29,22 @@ const ProductColor = () => {
     <div className="product_color_wrapper">
       <h3>Colours</h3>
       <ul className="flex-row">
-        {colors.map(color => {
-          <li>
-            <Link to="/products/productId">
-              <input type="radio"
-                name="color"
-                value={color}
-                id={`color ${color}`}
-                onChange={handleColor}
-              />
-              <label for={`color ${color}`}>
-                <img src={colorUrl} 
-                  alt={product} />
-                {color}
-              </label>
-            </Link>
-          </li>
-        })}
+        {colors.map(color =>
+        <li>
+          <input type="radio"
+            name="color"
+            value={color}
+            id={`color ${color}`}
+            onChange={handleColor}
+          />
+          <label for={`color ${color}`}>
+            <img 
+              src={`https://reels-dev.s3.us-west-1.amazonaws.com/${name}/${name}_${color}_color.webp`}
+              alt={`${name} ${color}`}
+            ></img>
+          </label>
+        </li>
+        )}
       </ul>
     </div>
   )
