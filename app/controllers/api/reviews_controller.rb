c6ceab3741c403680d2912d057ea9f61b2cc82e5
fee3fitri <safitri.shelton@gmail.com>
2022-11-09 10:49:45 -0800

    Remove generateValue from render.yaml

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,8 +1,9 @@
-class Api:ReviewsController < ApplicationController
+class Api::ReviewsController < ApplicationController
   before_action :require_logged_in, only: [:create, :update, :destroy]
 
   def create
-    @review= Review.new(review_params)
+    debugger
+    @review = Review.new(review_params)
 
     if @review.save
       render :show
