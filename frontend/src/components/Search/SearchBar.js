51caf8689ca65a90b35cd57bb917f4992319c320
fee3fitri <safitri.shelton@gmail.com>
2022-12-11 02:05:20 -0800

    Fix persisted query on search bar

--- frontend/src/components/Search/SearchBar.js
+++ frontend/src/components/Search/SearchBar.js
@@ -1,14 +1,24 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Search.css';
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    history.push({
+      pathname: '/search-results', 
+      state: {query}
+    });
+    setQuery("");
+  }
 
   return (
     <div className="search_bar_wrapper flex-row align-center">
       <form className='search_form flex-row align-center'
-        onSubmit={e => e.preventDefault()}>
+        onSubmit={handleSubmit}>
         <input type="search"
           value={query}
           placeholder="Search product"
